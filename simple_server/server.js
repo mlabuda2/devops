2ee43b2b28757fd53b61c78ef14eae5f1ca5a089
mati <mateusz.labuda@voicetel.pl>
2020-04-05 09:46:38 +0200

    init simple server redis+node

---
+++ simple_server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const redis = require("redis");
+const process = require("process");
+const app = express();
+const client = redis.createClient({
+    host: 'myredis-server',
+    port: 6379
+});
+
+client.set('counter', 0);
+
+app.get('/', (req, res)=>{
+    res.send('Hello world');
+});
+
+app.listen(8080, ()=>{
+    console.log("Listening on port 8080");
+})
